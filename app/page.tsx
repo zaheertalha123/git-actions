d7a364101c03fbf47cee484351bc98db36b61857
zaheertalha123 <F2021266557@umt.edu.pk>
2025-06-02 16:28:55 +0500

    Updated Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            This is Test
           </li>
         </ol>
 
